624b93ed5136f63b378257f1411e3dba7a9ede3d
Jen Hamon <jhamon@gmail.com>
2013-11-26 15:19:16 -0800

    Add some LinkedList specs.

--- spec/linked_list_spec.rb
+++ spec/linked_list_spec.rb
@@ -29,6 +29,48 @@ describe "RegularNode" do
   it { should respond_to(:pointer) }
   it { should respond_to(:value=) }
   it { should respond_to(:pointer=) }
-
 end
 
+
+describe "Linked lists" do
+  subject(:linked_list) { LinkedList.new }
+
+  it { should respond_to(:[]) }
+  it { should respond_to(:[]=) }
+  it { should respond_to(:<<) }
+  it { should respond_to(:push) }
+  it { should respond_to(:pop) }
+  it { should respond_to(:root) }
+
+  it "should begin with length zero" do
+    expect(linked_list.length).to eq(0)
+  end
+
+  before(:each) do
+    linked_list << 5
+    linked_list << 4
+    linked_list << :hello
+    linked_list << :world
+  end
+
+  describe "#push" do
+    it "pushing should increase the list length" do
+      expect(linked_list.length).to eq(4)
+    end
+
+    it "pushing should add elements to the end of the linked list" do
+      expect(linked_list.to_s).to eq("[5, 4, :hello, :world]")
+    end
+  end
+
+  describe "#pop" do
+    it "decreases the list length" do
+      linked_list.pop
+      expect(linked_list.length).to eq(3)
+    end
+
+    it "returns the last element" do
+      expect(linked_list.pop).to eq(:world)
+    end
+  end
+end
