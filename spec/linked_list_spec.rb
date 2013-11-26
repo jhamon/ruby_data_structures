37bd06e0bb3d3edc9458229896e0cfb248d13909
Jen Hamon <jhamon@gmail.com>
2013-11-26 15:10:12 -0800

    Add node specs.

---
+++ spec/linked_list_spec.rb
@@ -0,0 +1,34 @@
+require 'rspec'
+require 'linked_list.rb'
+
+describe "Sentinal nodes" do
+  subject (:sentinel) { SentinelNode.new }
+
+  it "should be a kind of Node" do
+    expect(sentinel).to be_a(Node)
+  end
+
+  it "should not have a pointer to any other nodes" do
+    expect(sentinel.pointer).to be_nil
+  end
+
+  it "should not store value" do
+    expect(sentinel.value).to eq(:sentinel)
+  end
+
+  it "should not allow reassigment of its pointer" do
+    expect(sentinel).not_to respond_to(:pointer=)
+  end
+end
+
+describe "RegularNode" do
+  let(:sentinel) { SentinelNode.new }
+  subject(:node) { RegularNode.new(5, sentinel) }
+
+  it { should respond_to(:value) }
+  it { should respond_to(:pointer) }
+  it { should respond_to(:value=) }
+  it { should respond_to(:pointer=) }
+
+end
+
