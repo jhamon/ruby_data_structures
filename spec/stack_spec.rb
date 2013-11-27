c88dd2a57c2ce2e40644556d42afbed97a46a1fd
Jen Hamon <jhamon@gmail.com>
2013-11-27 13:42:23 -0800

    Add basic Stack specs.

---
+++ spec/stack_spec.rb
@@ -0,0 +1,57 @@
+require 'rspec'
+require 'stack.rb'
+
+describe "Stack" do
+  subject(:stack) { Stack.new }
+  it { should respond_to (:pop) }
+  it { should respond_to (:push) }
+  it { should respond_to (:empty?) }
+  
+  context "an empty stack" do
+    it "has size zero" do
+      expect(stack.length).to eq(0)
+      expect(stack.size).to eq(0)
+    end
+
+    it "returns nil when popped" do
+      expect(stack.pop).to be_nil
+    end
+
+    it "knows that it is #empty?" do
+      expect(stack.empty?).to be(true)
+    end
+  end
+
+  context "a non-empty stack" do
+    before(:each) do 
+      stack << 3
+      stack << 2
+      stack << 1
+    end
+
+    it "has a length property" do
+      expect(stack.length).to eq(3)
+    end
+
+    context "when popped" do
+      it "decreases its length" do
+        stack.pop
+        expect(stack.length).to eq(2)
+      end
+
+      it "returns the last element pushed (FIFO)" do
+        expect(stack.pop).to eq(1)
+      end
+    end
+
+    context "when pushed" do
+      it { should respond_to(:<<) }
+      
+      it "should increase the stack length by one" do
+        start_length = stack.length
+        stack << 0
+        expect(stack.length).to eq(start_length + 1)
+      end
+    end
+  end
+end
