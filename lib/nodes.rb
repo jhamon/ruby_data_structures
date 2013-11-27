d37292df44c89a719fe7864e42bcd4a940d6a133
Jen Hamon <jhamon@gmail.com>
2013-11-27 11:14:20 -0800

    Add LinkedListNode.

---
+++ lib/nodes.rb
@@ -0,0 +1,28 @@
+class Node
+end
+
+class SinglyLinkedNode < Node
+  attr_reader :value, :pointer
+
+  def initialize
+    raise "Abstract base class called."
+  end
+end
+
+class SentinelNode < SinglyLinkedNode
+  def initialize
+    @value = :sentinel
+    @pointer = nil
+  end
+end
+
+class LinkedListNode < SinglyLinkedNode
+  attr_writer :value, :pointer
+
+  def initialize(value, parent_node)
+    @value = value
+    raise "Invalid node reference" unless parent_node.is_a?(Node)
+    @pointer = parent_node
+  end
+end
+
